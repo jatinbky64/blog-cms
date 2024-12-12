191ac1566f1b49fe2da7d9e35e6ea0262c35b9ca
jatinbky64 <jatin@odinschool.com>
2024-12-12 17:50:58 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",  // Make sure this pattern includes all your React components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
